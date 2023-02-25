3483e212a2299ba73a77e48f5945704ca1584e8a
cmstar <cosmo_sei@hotmail.com>
2023-02-26 00:38:27 +0800

    Init headers package.

---
+++ headers/headers_a_g.go
@@ -0,0 +1,6 @@
+// Package headers package provides constants of HTTP headers.
+//
+// The list of HTTP headers is taken from https://en.wikipedia.org/wiki/List_of_HTTP_header_fields .
+package headers
+
+/* From letter A to G. */
